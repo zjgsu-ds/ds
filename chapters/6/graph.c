6f36ddea6e96258adf4fbb77df11be57468df1e2
Jianwei Han <hanjianwei@gmail.com>
2014-04-29 18:45:53 +0800

    Add chapter 6

---
+++ chapters/6/graph.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct graph_container {
+    char *pVertex;
+    float *pEdge;
+    int n;
+};
+
+void InitGraph(struct graph_container *pGraph);
+void CreateGraph(struct graph_container *pGraph);
+int VertexDegree(struct graph_container *pGraph, int i);
+int GraphEdgeNumber(struct graph_container *pGraph);
+
+int main()
+{
+    // 一个图容器
+    struct graph_container g;
+
+    int iV = 1;
+
+    // 初始化该容器
+    InitGraph(&g);
+
+    // 构建一个图:依据图结构把数据放入容器中
+    CreateGraph(&g);
+
+    // 求第一个顶点的度
+    printf("The degree of vertex %d is: %d\n", iV, VertexDegree(&g, iV));
+
+    // 求图中边的条数
+    printf("The number of graph edges is: %d\n", GraphEdgeNumber(&g));
+
+    return 0;
+}
+
+void InitGraph(struct graph_container *pGraph)
+{
+    pGraph->pVertex = 0;
+    pGraph->pEdge = 0;
+    pGraph->n = 0;
+}
+
+void CreateGraph(struct graph_container *pGraph)
+{
+    int i, j, k, e, n;
+    float w;
+
+    // 读入图中顶点数和边数
+    scanf("%d%d\n", &n, &e);
+
+    // 分配图的数据存储空间
+    pGraph->n = n;
+    pGraph->pVertex = (char *)malloc(sizeof(char)*n);
+    pGraph->pEdge = (float *)malloc(sizeof(float)*n*n);
+
+    // 初始化邻接矩阵
+    for(i = 0; i < n; i ++)
+        for(j = 0; j < n; j ++)
+            pGraph->pEdge[i*n+j] = 0;
+
+    // 读入顶点数据
+    for(i = 0; i < n; i ++)
+        pGraph->pVertex[i] = getchar();
+
+    // 读入边或其权值
+    for(k = 0; k < e; k ++) {
+        scanf("%d%d%f", &i, &j, &w);
+        pGraph->pEdge[i*n+j] = w;
+    }
+}
+
+int VertexDegree(struct graph_container *pGraph, int i)
+{
+    int degree = 0, k;
+
+    for(k = 0; k < pGraph->n; k ++) {
+        if(pGraph->pEdge[(i-1)*pGraph->n + k] != 0) {
+            degree ++;
+        }
+    }
+
+    return degree;
+}
+
+int GraphEdgeNumber(struct graph_container *pGraph)
+{
+    int e = 0, i;
+
+    for(i = 0; i < pGraph->n*pGraph->n; i ++) {
+        if(pGraph->pEdge[i] != 0) {
+            e ++;
+        }
+    }
+
+    return e/2;
+}
+
---
+++ chapters/6/index.md
@@ -0,0 +1,28 @@
+---
+layout: page
+title: 6 图
+---
+
+## 课件
+
+[下载](chap6.ppt)
+
+## 代码
+
+[下载](graph.c)
+
+## 实验
+
+[图](graph.doc)
+
+## 可视化演示
+
+- [广度遍历](http://www.cs.usfca.edu/~galles/visualization/BFS.html)
+- [深度遍历](http://www.cs.usfca.edu/~galles/visualization/DFS.html)
+- [联通分量](http://www.cs.usfca.edu/~galles/visualization/ConnectedComponent.html)
+- [Dijkstra最短路径算法](http://www.cs.usfca.edu/~galles/visualization/Dijkstra.html)
+- [Prim最小生成树](http://www.cs.usfca.edu/~galles/visualization/Prim.html)
+- [拓扑排序（用入度）](http://www.cs.usfca.edu/~galles/visualization/TopoSortIndegree.html)
+- [拓扑排序（深度遍历）](http://www.cs.usfca.edu/~galles/visualization/TopoSortDFS.html)
+- [Floyd最短路径算法](http://www.cs.usfca.edu/~galles/visualization/Floyd.html)
+- [Kruskal最小生成树](http://www.cs.usfca.edu/~galles/visualization/Kruskal.html)
